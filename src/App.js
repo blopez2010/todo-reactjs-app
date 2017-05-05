54571172f7a03a40748b93af55fa326c02702aa3
Byron Lopez <merlin2010@gmail.com>
2017-05-05 16:07:23 -0600

    Adding empty current todo message validation.

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleEmptySubmit = this.handleEmptySubmit.bind(this)
   }
 
   handleInputChange(e) {
@@ -33,11 +34,20 @@ class App extends Component {
     const updatedTodos = addTodo(this.state.todos, newTodo)
     this.setState({
       todos: updatedTodos,
-      currentTodo: ''
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit(e) {
+    e.preventDefault()
+    this.setState({
+      errorMessage: 'Please supply a todo name'
     })
   }
 
   render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit;
     return (
       <div className="App">
         <div className="App-header">
@@ -45,9 +55,10 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
           <TodoForm handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo}
-            handleSubmit={this.handleSubmit}/>
+            handleSubmit={submitHandler}/>
           <TodoList todos={this.state.todos} />
         </div>
       </div>
