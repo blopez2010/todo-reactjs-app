d0779cefb28f18f046f05adffdd8415cb34d67fb
Byron Lopez <merlin2010@gmail.com>
2017-05-10 17:20:27 -0600

    Adding router to the app.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { TodoForm, TodoList } from './components/todo';
-import { addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo } from './lib/todoHelpers';
+import { TodoForm, TodoList, Footer } from './components/todo';
+import { addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos } from './lib/todoHelpers';
 import {pipe, partial} from './lib/utils';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   state = {
@@ -13,6 +14,10 @@ class App extends Component {
       { id: 3, name: 'Ship It', isComplete: false }
     ],
     currentTodo: ''
+  }
+
+  static contextTypes = {
+    route: PropTypes.string
   }
 
   handleRemove = (id, e) => {
@@ -56,6 +61,7 @@ class App extends Component {
 
   render() {
     const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit;
+    const displayTodos = filterTodos(this.state.todos, this.context.route)
     return (
       <div className="App">
         <div className="App-header">
@@ -69,8 +75,9 @@ class App extends Component {
             handleSubmit={submitHandler} />
           <TodoList
             handleToggle={this.handleToggle}
-            todos={this.state.todos}
+            todos={displayTodos}
           handleRemove={this.handleRemove} />
+          <Footer />
         </div>
       </div>
     );
